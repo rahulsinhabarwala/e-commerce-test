e6f65f8def545a0fa31f2cc6b96f4442d39337a1
rahulsinhabarwala <rahulsinhabarwala@gmail.com>
2019-02-04 16:25:40 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,25 +1,49 @@
-# Sass Starter Pack
+# :zap: Gulp Boilerplate
 
-A light package for compiling Sass and running a dev server
+## Instructions for the boilerplate
 
-### Version
-1.0.0
+* Clone the repository.
 
-## Usage
+* Use this command to install all dependencies for the project:
 
-
-### Installation
-
-Install the dependencies (gulp, gulp-sass, browser-sync)
-
-```sh
-$ npm install
+```bash
+# Uses package.json "devDependencies" to install dependencies
+npm install
 ```
 
-### Run
+* Start `gulp` by running:
 
-This will watch your sass files, compile them and run your dev server at http://localhost:3000
-
-```sh
-$ npm start
+```bash
+npm start
 ```
+
+* Write code in the `src`-folder.
+
+
+## Folder structure
+
+* `dist` - The converted files that are linked via `index.html`. You don't have to touch these files
+    - `js` - All converted `js`-files go here
+    - `css` - All converted `css`-files go here
+* `src` - This is where your development-files are. There are the ones you work on
+    - `js` - All unconverted `js`-files
+    - `scss` - All unconverted `scss`-files
+* `Gulpfile.js` - This config file must be in the root-folder
+* `index.html` - The index must be in the root-folder
+* `package.json` - All the dependencies and config for the project
+
+
+## Dependencies used in this boilerplate
+
+* [gulp](https://www.npmjs.com/package/gulp)
+* [gulp-sass](https://www.npmjs.com/package/gulp-sass)
+* [gulp-rename](https://www.npmjs.com/package/gulp-rename)
+* [browser-sync](https://www.npmjs.com/package/browser-sync)
+* [gulp-postcss](https://www.npmjs.com/package/gulp-postcss)
+* [autoprefixer](https://www.npmjs.com/package/autoprefixer)
+* [gulp-babel](https://www.npmjs.com/package/gulp-babel)
+* [babel-preset-env](https://www.npmjs.com/package/babel-preset-env)
+* [browserify](https://www.npmjs.com/package/browserify)
+* [babelify](https://www.npmjs.com/package/babelify)
+* [gulp-util](https://www.npmjs.com/package/gulp-util)
+* [vinyl-source-stream](https://www.npmjs.com/package/vinyl-source-stream)
--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,54 @@
-const gulp         = require('gulp');
-const browserSync  = require('browser-sync').create();
-const sass         = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
+//utils
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const gutil = require('gulp-util');
+const rename = require('gulp-rename');
 
-// Compile Sass & Inject Into Browser
-gulp.task('sass', function() {
-    return gulp.src(['src/scss/*.scss'])
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest("src/css"))
+//css
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+
+//babel + browserify
+const babel = require('gulp-babel');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+
+
+gulp.task('default', ['sass', 'babel'], () => {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
+    gulp.watch('./src/js/*.js', ['babel']);
+});
+
+gulp.task('sass', () => {
+    const plugins = [autoprefixer({browsers: ['last 2 versions']}), cssnano()];
+    return gulp.src('./src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(plugins))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
 });
 
 
-// Watch Sass & Serve
-gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-        server: "./src"  
-    });
-
-    gulp.watch(['src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+gulp.task('babel', function() {
+    browserify({
+        entries: './src/js/main.js',
+        debug: true
+    })
+    .transform(babelify, { presets: ['env'] })
+    .on('error',gutil.log)
+    .bundle()
+    .on('error',gutil.log)
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream());
 });
-
-// Default Task
-gulp.task('default', ['serve']);
