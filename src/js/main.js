d7bdb5c45c8bf95937b30d0566446b4984e9c621
rahulsinhabarwala <rahulsinhabarwala@gmail.com>
2019-02-06 11:18:48 +0530

    initial

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,21 @@
 $('#blogCarousel').carousel({
     interval: 5000
 });
+
+
+// $('.carousel[data-type="multi"] .item').each(function(){
+//     var next = $(this).next();
+//     if (!next.length) {
+//       next = $(this).siblings(':first');
+//     }
+//     next.children(':first-child').clone().appendTo($(this));
+  
+//     for (var i=0;i<4;i++) {
+//       next=next.next();
+//       if (!next.length) {
+//           next = $(this).siblings(':first');
+//         }
+      
+//       next.children(':first-child').clone().appendTo($(this));
+//     }
+//   });
