bdac9725d0d4507ef1c2399743f97ef7925c3923
rahulsinhabarwala <rahulsinhabarwala@gmail.com>
2019-02-22 11:15:52 +0530

    near final

--- dist/js/slick.js
+++ dist/js/slick.js
@@ -3079,3 +3079,38 @@ $('.sliderProd').slick({
 
     }]
   });
+
+  $('.slickspec').slick({
+    dots: true,
+    infinite: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 400,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }, {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      } // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+
+    }]
+  });
